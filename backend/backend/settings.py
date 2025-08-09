49c17c304fdbc26d6a323b71472e87cddc3cdc3c
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-08-10 00:12:19 +0530

    deploying on render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,18 +105,28 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+
+#         'HOST': 'gondola.proxy.rlwy.net',
+
+#         'PORT': '51173',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-
-        'USER': 'postgres',
-
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
-
-        'HOST': 'gondola.proxy.rlwy.net',
-
-        'PORT': '51173',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
