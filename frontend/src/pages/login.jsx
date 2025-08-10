4d8b6d919818cc5794ab25bf42e9b308e3a624bf
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-08-10 12:58:28 +0530

    update

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,12 @@
 import Form from "../components/form"
+import { Link } from "react-router-dom"
 
 export default function Login(){
     return (
+        <div>
         <Form route ='/api/token/' method="login" />
+        <Link to ='/register'></Link>
+        </div>
+
     )
 }
